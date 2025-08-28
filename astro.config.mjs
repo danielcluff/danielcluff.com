88319d9102211539216ca3ea6dbec94096fc4975
Daniel <58708714+danielcluff@users.noreply.github.com>
2025-08-28 11:01:31 -0600

    adding solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [solidJs()],
     vite: {
         plugins: [tailwindcss()],
     },
